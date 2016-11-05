ef92f6a5c30fdc2cc219dea69a87d2b3cf779794
Allan Ritchie <allan.ritchie@gmail.com>
2016-11-04 22:00:33 -0400

    minor

--- Acr.Ble.Server.Android/GattCharacteristic.cs
+++ Acr.Ble.Server.Android/GattCharacteristic.cs
@@ -14,7 +14,7 @@ namespace Acr.Ble.Server
     public class GattCharacteristic : AbstractGattCharacteristic
     {
         public static readonly Guid NotifyDescriptorId = new Guid("00002902-0000-1000-8000-00805f9b34fb");
-        public static readonly UUID NotifyDescriptorUuid = Java.Util.UUID.FromString("00002902-0000-1000-8000-00805f9b34fb");
+        public static readonly UUID NotifyDescriptorUuid = UUID.FromString("00002902-0000-1000-8000-00805f9b34fb");
         public static readonly byte[] NotifyEnabledBytes = BluetoothGattDescriptor.EnableNotificationValue.ToArray();
         public static readonly byte[] NotifyDisableBytes = BluetoothGattDescriptor.DisableNotificationValue.ToArray();
 
