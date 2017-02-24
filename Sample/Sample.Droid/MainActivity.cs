c9a9c14d3d79ae5c37291aa58945cf60fe0b8ee1
Allan Ritchie <allan.ritchie@gmail.com>
2017-02-24 14:49:44 -0500

    adding samples

---
+++ Sample/Sample.Droid/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+
+
+namespace Sample.Droid
+{
+	[Activity(
+        Label = "Sample",
+        Icon = "@drawable/icon",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation
+    )]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Forms.Init (this, bundle);
+			this.LoadApplication(new Sample.App());
+		}
+	}
+}
+
---
+++ Sample/Sample/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Samples.Pages.Server;
+using Xamarin.Forms;
+
+
+namespace Samples
+{
+    public class App : Application
+    {
+        public App()
+        {
+            this.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
